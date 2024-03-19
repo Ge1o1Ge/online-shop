8c8c72f0379aefc770bf697c7f68270d22cefb62
Georg II <trajtv@gmail.com>
2024-03-19 22:33:09 +0300

    feat: brands section

---
+++ rostelecom-shop/online-shop/components/modules/QuickViewModal/QuickViewModal.tsx
@@ -0,0 +1,13 @@
+import styles from '@/styles/quick-view-modal/index.module.scss'
+
+const QuickViewModal = () => {
+  console.log()
+
+  return (
+    <div className={styles.modal}>
+      <button className={styles.modal__close} />
+    </div>
+  )
+}
+
+export default QuickViewModal
